d9ca928ff8bb9beef7e2d61a113514d742b93d4e
mink <use1017@naver.com>
2023-11-03 17:20:06 +0900

    Create YoilTeller.java-Terminal로 실행하기

--- ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTeller.java
@@ -1,45 +1,29 @@
 package com.fastcampus.ch2;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Calendar;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-//³â¿ùÀÏÀ» ÀÔ·ÂÇÏ¸é ¿äÀÏÀ» ¾Ë·ÁÁÖ´Â ÇÁ·Î±×·¥
-@Controller
+//ë…„ì›”ì¼ì„ ì…ë ¥í•˜ë©´ ìš”ì¼ì„ ì•Œë ¤ì£¼ëŠ” í”„ë¡œê·¸ë¨
 public class YoilTeller {
-
-	//public static void main(String[] args) {
-	@RequestMapping("/getYoil")
-	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		//1.ÀÔ·Â
-		String year = request.getParameter("year");
-		String month = request.getParameter("month");
-		String day = request.getParameter("day");
+	public static void main(String[] args) {
+		//1. ì…ë ¥
+		String year = args[0];
+		String month = args[1];
+		String day = args[2];
   
-		//¹®ÀÚ¿­ ¼ıÀÚ·Î º¯È¯
 		int yyyy = Integer.parseInt(year);
 		int mm = Integer.parseInt(month);
 		int dd = Integer.parseInt(day);
 
-		//2.ÀÛ¾÷
+		//2. ì‘ì—…
 		Calendar cal = Calendar.getInstance();
 		cal.set(yyyy,mm - 1, dd);
 
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);//1:ÀÏ¿äÀÏ, 2:¿ù¿äÀÏ ...
-		char yoil = " ÀÏ¿ùÈ­¼ö¸ñ±İÅä".charAt(dayOfWeek);
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);//1:ì¼ìš”ì¼, 2: ì›”ìš”ì¼ ...
+		char yoil = " ì¼ì›”í™”ìˆ˜ëª©ê¸ˆí† ".charAt(dayOfWeek);
 
-		//3.Ãâ·Â
-		response.setContentType("text/html");
-		response.setCharacterEncoding("utf-8");
-		PrintWriter out = response.getWriter();//response°´Ã¼¿¡¼­ ºê¶ó¿ìÀú·ÎÀÇ Ãâ·Â ½ºÆ®¸²À» ¾ò´Â´Ù.
-		out.println(year + "³â " + month + "¿ù "+ day + "ÀÏÀº ");
-		out.println(yoil + "¿äÀÏÀÔ´Ï´Ù.");
+		//3. ì¶œë ¥
+		System.out.println(year + "ë…„ " + month + "ì›” "+ day + "ì¼ì€ ");
+		System.out.println(yoil + "ìš”ì¼ì…ë‹ˆë‹¤.");
 	}
 
 }
