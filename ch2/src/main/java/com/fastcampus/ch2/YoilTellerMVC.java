61254fe89e761ab66ff190b91dfe57be4cdf3948
mink <use1017@naver.com>
2023-11-15 18:34:19 +0900

    Create MethodCall2.java

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -13,36 +13,33 @@ import org.springframework.web.servlet.ModelAndView;
 public class YoilTellerMVC {
 //	public void main(HttpServletRequest request, HttpServletResponse response) throws IOException { //http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1
 	@RequestMapping("/getYoilMVC")
-		public ModelAndView main(int year, int month, int day) throws IOException {
+	 public String main(int year, int month, int day, Model model) throws IOException {
 		
-		ModelAndView mv = new ModelAndView();
+		//ModelAndView mv = new ModelAndView();
 
-		//1. 유효성 검사
-//		if(!isVaild(year, month, day)) {
-//			return "yoilError";
-//		}
-		//2. 요일 계산
-		char yoil = getYoil(year, month, day);
-		
-		
-		//3. 계산한 결과를 model에 저장
-		mv.addObject("year", year);
-		mv.addObject("month", month);
-		mv.addObject("day", day);
-		mv.addObject("yoil", yoil);
-		
-		//4. 결과를 보여줄 view를 지정
-		mv.setViewName("yoil");
-		
-		return mv;
-		
-//		return "yoil"; // /WEB-INF/views/yoil.jsp
+		 // 1. 유효성 검사
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    	
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
+
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+        
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
 
 	}
 
-	private boolean isVaild(int year, int month, int day) {
-		return true;
-	}
+	 private boolean isValid(int year, int month, int day) {    
+	    	if(year==-1 || month==-1 || day==-1) 
+	    		return false;
+	    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+	 }
 
 	private char getYoil(int year, int month, int day) {
 		Calendar cal = Calendar.getInstance();
