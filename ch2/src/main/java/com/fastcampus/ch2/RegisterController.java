45475f3654c68f2c0eff55dd8847815a26fd0f6d
mink <use1017@naver.com>
2023-11-29 16:44:17 +0900

    CREATE LoginController.java

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -5,15 +5,15 @@ import java.net.URLEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-//	@RequestMapping(value="/register/save", method={RequestMethod.GET, RequestMethod.POST})
-//	@RequestMapping("/register/add")//신규 회원 가입 화면
-//	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm";
-//	}
+	@RequestMapping(value="/register/add", method={RequestMethod.GET, RequestMethod.POST})
+	public String register() {
+		return "registerForm";
+	}
 	
 //	@RequestMapping(value="/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save")
@@ -22,7 +22,7 @@ public class RegisterController {
 		if(!isValid(user)) {
 			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
 			m.addAttribute("msg", msg);
-			return "redirect:/register/add";
+			return "forward:/register/add";
 //			return "redirect:/register/add?msg="+msg;//URL재작성(rewriting)
 		}
 		//2. DB에 신규회원 정보를 저장
@@ -30,7 +30,7 @@ public class RegisterController {
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 
 }
