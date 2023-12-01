d50f3da999e65e6345fd2f114688698f3700e6a4
mink <use1017@naver.com>
2023-12-01 17:21:55 +0900

    CREATE BoardController.java

--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -3,7 +3,10 @@ package com.fastcampus.ch2;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +20,18 @@ public class LoginController {
 	public String loginForm() {
 		return "loginForm";
 	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		//1. 세션을 종료
+		session.invalidate();
+		//2. 홈으로 이동
+		return "redirect:/";
+	}
 
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
-		System.out.println("id=" + id);
-		System.out.println("pwd" + pwd);
-		System.out.println("rememberId=" + rememberId);
+	public String login(String id, String pwd, boolean rememberId, 
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
 //		1. id와 pwd를 확인
 		if (!loginCheck(id, pwd)) {
 //		2-1. 일치하지 않으면, loginForm으로 이동.
@@ -31,6 +40,11 @@ public class LoginController {
 			return "redirect:/login/login?msg=" + msg;
 		}
 //		2-2. id와 pwd가 일치하면,
+		//세션 객체를 얻어오기
+		HttpSession session = request.getSession();
+		//세션 객체에 id를 저장
+		session.setAttribute("id", id);
+		
 		if (rememberId) {
 			// 쿠키를 생성
 //		1. 쿠키를 생성
